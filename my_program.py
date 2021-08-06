1a132450778c72ff82493ff3a04e9e22ef4bdc7e
zioan <zaharia.ioan@gmail.com>
2021-08-06 17:33:48 +0200

    bug fux

--- my_program.py
+++ my_program.py
@@ -2,11 +2,11 @@
 def sentence_maker(phrase):
   interrogatives = ("how", "what", "why")
   capitalized = phrase.capitalize()
-  #if phrase.startswith(("how", "what", "why"))
+ #if phrase.startswith(("how", "what", "why"))
   if phrase.startswith(interrogatives):
     return "{}?".format(capitalized)
     #this will capitalize the first letter and also add a ? at the end on the string
   else:
     return "{}.".format(capitalized)
     
-  print(sentence_maker("how are you"))
+print(sentence_maker("yes please"))
